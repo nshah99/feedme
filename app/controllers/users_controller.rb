1f1baddb2392b12e839f19443551ce4d77247804
Nishant <nshah99@gatech.edu>
2013-10-23 18:05:45 -0400

    Add capability to update user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,18 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def delete
@@ -30,6 +42,6 @@ class UsersController < ApplicationController
   
   private
   def user_params
-      params.require(:user).permit(:email,:password,:name,:password_confirmation,:picture)
+      params.require(:user).permit(:email,:password,:name,:password_confirmation,:picture,:description,:cuisines)
   end
 end
